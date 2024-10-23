11e1d22c7e1231d174b92739d909c92e7c24bc1b
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2024-10-23 22:19:25 +0530

    feat: adding ethena vault rateproviders

--- src/RateProvider/ICurveStableSwapNG.sol
+++ src/RateProvider/ICurveStableSwapNG.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.24;
 
 interface ICurveStableSwapNG {
     function get_virtual_price() external view returns (uint256);
+    function stored_rates() external view returns (uint256[] memory);
+    function price_oracle(uint256 i) external view returns (uint256);
 }
