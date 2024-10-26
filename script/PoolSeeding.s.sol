fbe3de408eeed41a081f7832e006d3ddefc4abec
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2024-10-26 19:48:01 +0530

    feat: added pool seeding scripts

--- script/PoolSeeding.s.sol
+++ script/PoolSeeding.s.sol
@@ -17,6 +17,7 @@ import {PoolEstimator} from "../tests/PoolEstimator.sol";
 import {LogExpMath} from "../src/BalancerLibCode/LogExpMath.sol";
 import {IRateProvider} from "../src/RateProvider/IRateProvider.sol";
 import {UsdeVaultRateProvider} from "../src/RateProvider/ethena-usde/EthenaVaultRateProvider.sol";
+import {StakeeaseVaultRateProvider} from "../src/RateProvider/stakeease-sxeth/StakeeaseVaultRateProvider.sol";
 
 contract PoolSeeding is Script {
     Pool pool = Pool(0x001DF2Cc0c3433beAd3703575F13841d2EBC078f);
@@ -84,6 +85,63 @@ contract PoolSeeding is Script {
 
         poolToken.approve(address(vault), lpReceived);
         uint256 shares = vault.deposit(lpReceived, ADMIN_ADDRESS);
+        vault.transfer(ADMIN_ADDRESS, shares / 90);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract SeedSxETHPool is Script {
+    Pool pool = Pool(0x188B679c0bAf56b9838584AfaC82D713e68112fC);
+    PoolToken poolToken = PoolToken(0x34e523B10B85c41515807811456613Cf2a077C77);
+    MasterVault vault = MasterVault(0xCDE68b2DB42cfA27ad9A653eEAc4f23297227175);
+    MockRateProvider rateProvider = MockRateProvider(0x144Bec263C77E9d20946d2A8A96507d68c4922D5);
+
+    uint256 public amplification;
+    uint256 private PRECISION = 1e18;
+    address private ADMIN_ADDRESS = 0x1b514df3413DA9931eB31f2Ab72e32c0A507Cad5;
+
+    address private constant WSXETH = 0x082F581C1105b4aaf2752D6eE5410984bd66Dd21;
+    address private constant SXETHWETH_CURVE = 0x8b0fb150FbA4fc25cd4f6F5bd8a8F6944ad65Af0;
+
+    MockToken token0 = MockToken(WSXETH);
+    MockToken token1 = MockToken(SXETHWETH_CURVE);
+
+    address[] public tokens = new address[](2);
+    uint256[] public weights = new uint256[](2);
+    address[] public rateProviders = new address[](2);
+
+    function run() public {
+        uint256 adminPk = vm.envUint("PRIVATE_KEY_1");
+        vm.startBroadcast(adminPk);
+
+        tokens[0] = address(token0);
+        tokens[1] = address(token1);
+
+        weights[0] = 90 * PRECISION / 100;
+        weights[1] = 10 * PRECISION / 100;
+
+        rateProviders[0] = address(rateProvider);
+        rateProviders[1] = address(rateProvider);
+
+        uint256 total = 0.05 ether;
+
+        uint256[] memory amounts = new uint256[](2);
+
+        // for (uint256 i = 0; i < 2; i++) {
+        //     address token = tokens[i];
+        //     require(MockToken(token).approve(address(pool), type(uint256).max), "could not approve");
+        //     uint256 amount = (total * weights[i]) / rateProvider.rate(token);
+        //     amounts[i] = amount;
+
+        //     console.log("amount of token", MockToken(token).symbol(), " to be added =", amount);
+        //     console.log("balance =", MockToken(token).balanceOf(ADMIN_ADDRESS));
+        // }
+
+        // uint256 lpReceived = pool.addLiquidity(amounts, 0, ADMIN_ADDRESS);
+
+        poolToken.approve(address(vault), poolToken.balanceOf(ADMIN_ADDRESS));
+        uint256 shares = vault.deposit(poolToken.balanceOf(ADMIN_ADDRESS), ADMIN_ADDRESS);
         vault.transfer(ADMIN_ADDRESS, shares / 90);
 
         vm.stopBroadcast();
