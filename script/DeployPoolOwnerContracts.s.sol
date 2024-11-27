1b0e7f4cf27d447e39b7b88755b8a52f90957397
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2024-11-27 15:51:25 +0530

    feat: deployed ethena vault poolOwner contract

--- script/DeployPoolOwnerContracts.s.sol
+++ script/DeployPoolOwnerContracts.s.sol
@@ -2,3 +2,15 @@
 pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
+import {PoolOwner} from "../src/PoolOwner.sol";
+
+contract PoolOwnerDeploymentScript is Script {
+    PoolOwner ownerContract;
+
+    function run() public {
+        uint256 adminPk = vm.envUint("PRIVATE_KEY_1");
+        vm.startBroadcast(adminPk);
+        ownerContract = new PoolOwner(0x001DF2Cc0c3433beAd3703575F13841d2EBC078f);
+        vm.stopBroadcast();
+    }
+}
